1f13b350761db7f5909c87115e064849d4faa098
Niima1985 <67212610+Niima1985@users.noreply.github.com>
2022-07-06 12:04:03 -0700

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//import the router that created in places.js
+app.use('/places', require('./controllers/places'))
+
+// Create the home page route
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+// Create the Wildcard route
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
-app.listen(process.env.PORT) // pull the PORT variable from the .env file
+app.listen(process.env.PORT)
