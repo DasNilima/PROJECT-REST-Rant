f151759112ffc9d20f8d3f11842e5a7a00d17a27
Niima1985 <67212610+Niima1985@users.noreply.github.com>
2022-07-08 18:22:43 -0700

    Adding places view with mock data

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,25 @@
 // Create and export an express.Router()
-const router = require('express').Router()
+const app = require('express').Router()
 
-//Create the first route in places.js 
-router.get('/', (req, res) => {
-    res.send('GET /places')
+//Create the index route in places.js 
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+        }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+        }]
+        
+        res.render('places/index', { places })
 })
 
-module.exports = router
+
+module.exports = app
--- index.js
+++ index.js
@@ -5,18 +5,22 @@ const express = require('express')
 //Initialized the app object
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 //import the router that created in places.js
 app.use('/places', require('./controllers/places'))
 
 // Create a homepage route
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 
 // Create a Wildcard route
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
+
 
 // listen to a port number defined by a local environment variable
 app.listen(process.env.PORT)
