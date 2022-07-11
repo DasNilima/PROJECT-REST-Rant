dd11f7814bb952e5bbca9a1ba3bc354614cdefd0
Niima1985 <67212610+Niima1985@users.noreply.github.com>
2022-07-11 11:59:25 -0700

    Jazzed up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,7 +6,30 @@ function new_form () {
 return (
     <Def>
         <main>
-        <h1>Add a New Place</h1>
+            <h1>Add a New Place</h1>
+            <form method="POST" action="/places">
+                <div className="form-group mb-3 mt-3">
+                    <label htmlFor="name">Place Name</label>
+                    <input className="form-control" id="name" name="name" required />
+                </div>
+                <div className="form-group mb-3 mt-3">
+                    <label htmlFor="pic">Place Picture</label>
+                    <input className="form-control" id="pic" name="pic" />
+                </div>
+                <div className="form-group mb-3 mt-3">
+                    <label htmlFor="city">City</label>
+                    <input className="form-control" id="city" name="city" />
+                </div>
+                <div className="form-group mb-3 mt-3">
+                    <label htmlFor="state">State</label>
+                    <input className="form-control" id="state" name="state" />
+                </div>
+                <div className="form-group mb-3 mt-3">
+                    <label htmlFor="cuisines">Cuisines</label>
+                    <input className="form-control" id="cuisines" name="cuisines" required />
+                </div>
+                <input className="btn btn-primary" type="submit" value="Add Place" />
+            </form>
         </main>
     </Def>
 )
